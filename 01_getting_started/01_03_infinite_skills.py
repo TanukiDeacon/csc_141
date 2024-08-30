eeb34de840707e0dd270375f15b425e492c11397
derian-arroyo <tanukideacon@gmail.com>
2024-08-30 09:45:53 -0400

    Add files via upload

---
+++ 01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,13 @@
+'''
+
+If I had infinite coding skills i would want to make a hack and slash game with a high level of customizability.
+I would make it a sci-fi with mecha with many different options for ranged and melee weapons and diffternt types of body frames 
+and armor options that change stats and looks. My main inspiration ofr this is the Devil may cry series and Armored core series.
+
+I would also like to make a 3D metroidvania. One of the reasons I would like to do this is because I haven't seen many 3D platforming 
+metroidvanias. It would be based inside an expansive cave system from which you have to escape.
+
+Finally I would want to make an online card game. I'v always wanted to make a card game and I have actually already started 
+working on it. 
+
+'''
