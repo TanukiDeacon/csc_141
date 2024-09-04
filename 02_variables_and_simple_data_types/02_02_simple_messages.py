8cea97ba2082481c83a0fe430ce27ef1ba27632f
derian-arroyo <tanukideacon@gmail.com>
2024-09-04 09:40:11 -0400

    Add files via upload

---
+++ 02_variables_and_simple_data_types/02_02_simple_messages.py
@@ -0,0 +1,7 @@
+message = "Hello world."
+
+print(message)
+
+message = "Hello python world."
+
+print (message)
---
+++ 02_variables_and_simple_data_types/02_10_favorite_number.py
@@ -0,0 +1,6 @@
+FAVORITE_NUMBER = "21"
+
+message = "My favorite number is " + FAVORITE_NUMBER
+
+print(message)
+
