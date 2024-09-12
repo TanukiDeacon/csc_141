9a5dc254393be00181da09c2c69f461df9ed7f55
derian-arroyo <tanukideacon@gmail.com>
2024-09-12 10:42:46 -0400

    Add files via upload

---
+++ 03_inroducing_lists/03_04_guest_lists.py
@@ -0,0 +1,5 @@
+
+dinner_guest = ["my first cat", "Yoko Taro", "Kentaro Miura"]
+
+for guest in dinner_guest:
+    print (f"Hello {guest} would you like to have dinner with me.")
---
+++ 03_inroducing_lists/03_11_intentional_error.py
@@ -0,0 +1,7 @@
+names = ['Derek', 'Justin', 'Kenneth']
+
+print (f"{names[1]}")
+print (f"{names[2]}")
+print (f"{names[3]}")
+
+# the error will happen because their is nothing in idex 3
